bfe5b6b1e9528c80287d9371f4abcbbb9c5068ef
MrDlop <72319098+MrDlop@users.noreply.github.com>
2023-01-20 20:21:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 level = Level(level_1, screen)
 start = False
+coins = 0
+CDR = 0
+WCDR = 10
 
 while True:
     for event in pygame.event.get():
@@ -21,17 +24,24 @@ while True:
                 coords = pygame.mouse.get_pos()
                 if coords[0] >= screen_width // 2:
                     level = Level(level_0, screen)
+                else:
+                    level = Level(level_1, screen)
 
                 start = True
 
     if start:
         screen.fill('grey')
-        level.run()
+        if level.run():
+            start = False
+            CDR = WCDR
     else:
         screen.fill('black')
         f1 = pygame.font.Font(None, 36)
         text1 = f1.render('1', 1, (255, 255, 255))
         text2 = f1.render('2', 1, (255, 255, 255))
+        if CDR > 0:
+            text3 = f1.render('YOU WIN', 1, (255, 0, 0))
+            screen.blit(text3, (screen_width // 2 - 20, screen_height // 2 - 40))
         screen.blit(text1, (screen_width // 2 - 20, screen_height // 2))
         screen.blit(text2, (screen_width // 2 + 20, screen_height // 2))
 
