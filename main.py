41666ea4e48937e92f3363b5efbce5aea602242a
MrDlop <72319098+MrDlop@users.noreply.github.com>
2023-01-20 18:23:03 +0500

    add preview

--- main.py
+++ main.py
@@ -9,15 +9,24 @@ pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 level = Level(level_0, screen)
+start = False
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            start = True
 
-    screen.fill('grey')
-    level.run()
+    if start:
+        screen.fill('grey')
+        level.run()
+    else:
+        screen.fill('black')
+        f1 = pygame.font.Font(None, 36)
+        text1 = f1.render('Start', 1, (255, 255, 255))
+        screen.blit(text1, (screen_width // 2, screen_height // 2))
 
     pygame.display.update()
     clock.tick(60)
