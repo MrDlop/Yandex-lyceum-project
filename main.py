086a85d1557363140778256fac77e57a52af0eb4
MrDlop <72319098+MrDlop@users.noreply.github.com>
2023-01-20 18:53:16 +0500

    add level

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pygame
 import sys
 from settings import *
 from level import Level
-from game_data import level_0
+from game_data import level_0, level_1
 
 # Pygame setup
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
-level = Level(level_0, screen)
+level = Level(level_1, screen)
 start = False
 
 while True:
@@ -17,7 +17,12 @@ while True:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            start = True
+            if not start:
+                coords = pygame.mouse.get_pos()
+                if coords[0] >= screen_width // 2:
+                    level = Level(level_0, screen)
+
+                start = True
 
     if start:
         screen.fill('grey')
@@ -25,8 +30,10 @@ while True:
     else:
         screen.fill('black')
         f1 = pygame.font.Font(None, 36)
-        text1 = f1.render('Start', 1, (255, 255, 255))
-        screen.blit(text1, (screen_width // 2, screen_height // 2))
+        text1 = f1.render('1', 1, (255, 255, 255))
+        text2 = f1.render('2', 1, (255, 255, 255))
+        screen.blit(text1, (screen_width // 2 - 20, screen_height // 2))
+        screen.blit(text2, (screen_width // 2 + 20, screen_height // 2))
 
     pygame.display.update()
     clock.tick(60)
