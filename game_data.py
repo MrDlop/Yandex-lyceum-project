f3466ddc5621b690c03e7052648fe2b325e4ff86
Erbi <73945030+Erbi-glitch@users.noreply.github.com>
2023-01-20 18:49:04 +0500

    Update game_data.py

--- game_data.py
+++ game_data.py
@@ -11,12 +11,12 @@ level_0 = {
     'grass': f'graphics/level/{i}/level_{i}._grass.csv'}
 i = 1
 level_1 = {
-    'terrain': f'graphics/level/{i}/level_{i}._terrain.csv',
-    'coins': f'graphics/level/{i}/level_{i}._coin.csv',
-    'fg palms': f'graphics/level/{i}/level_{i}._fg palms.csv',
-    'bg palms': f'graphics/level/{i}/level_{i}._bg palms.csv',
-    'crates': f'graphics/level/{i}/level_{i}._crates.csv',
-    'enemies': f'graphics/level/{i}/level_{i}._enemies.csv',
-    'constraints': f'graphics/level/{i}/level_{i}._constrains.csv',
-    'player': f'graphics/level/{i}/level_{i}._player.csv',
-    'grass': f'graphics/level/{i}/level_{i}._grass.csv'}
+    'terrain': f'graphics/level/{i}/level_{i}_terrain.csv',
+    'coins': f'graphics/level/{i}/level_{i}_coin.csv',
+    'fg palms': f'graphics/level/{i}/level_{i}_fg palms.csv',
+    'bg palms': f'graphics/level/{i}/level_{i}_bg palms.csv',
+    'crates': f'graphics/level/{i}/level_{i}_crates.csv',
+    'enemies': f'graphics/level/{i}/level_{i}_enemies.csv',
+    'constraints': f'graphics/level/{i}/level_{i}_constrains.csv',
+    'player': f'graphics/level/{i}/level_{i}_player.csv',
+    'grass': f'graphics/level/{i}/level_{i}_grass.csv'}
