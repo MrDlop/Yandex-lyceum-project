b010f79675b1666b93b447adf9c595cbabac5b7b
Erbi <73945030+Erbi-glitch@users.noreply.github.com>
2023-01-20 17:27:33 +0500

    Add files via upload

---
+++ game_data.py
@@ -0,0 +1,10 @@
+level_0 = {
+    'terrain': 'graphics/level/0/level_0._terrain.csv',
+    'coins': 'graphics/level/0/level_0._coin.csv',
+    'fg palms': 'graphics/level/0/level_0._fg palms.csv',
+    'bg palms': 'graphics/level/0/level_0._bg palms.csv',
+    'crates': 'graphics/level/0/level_0._crates.csv',
+    'enemies': 'graphics/level/0/level_0._enemies.csv',
+    'constraints': 'graphics/level/0/level_0._constrains.csv',
+    'player': 'graphics/level/0/level_0._player.csv',
+    'grass': 'graphics/level/0/level_0._grass.csv'}
