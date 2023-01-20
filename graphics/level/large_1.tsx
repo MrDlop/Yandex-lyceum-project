d5f4ee1bb92d154cd27d34bb5bb75b597b60e8fc
MrDlop <72319098+MrDlop@users.noreply.github.com>
2023-01-20 17:44:00 +0500

    Add files via upload

---
+++ graphics/level/large_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="large_1" tilewidth="64" tileheight="64" tilecount="2" columns="1">
+ <image source="graphics/palm_large/large_1.png" width="78" height="136"/>
+</tileset>
---
+++ graphics/level/level_data/Palms.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Palms" tilewidth="128" tileheight="136" tilecount="13" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="78" height="136" source="../graphics/palm_large/large_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="78" height="136" source="../graphics/palm_large/large_2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="78" height="136" source="../graphics/palm_large/large_3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="78" height="136" source="../graphics/palm_large/large_4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="80" height="103" source="../graphics/Plam/small_3.png"/>
+ </tile>
+ <tile id="5">
+  <image width="80" height="103" source="../graphics/Plam/small_4.png"/>
+ </tile>
+ <tile id="6">
+  <image width="80" height="103" source="../graphics/Plam/small_1.png"/>
+ </tile>
+ <tile id="7">
+  <image width="80" height="103" source="../graphics/Plam/small_2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../graphics/palm_bg/bg_palm_3.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../graphics/palm_bg/bg_palm_4.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../graphics/palm_bg/bg_palm_1.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../graphics/palm_bg/bg_palm_2.png"/>
+ </tile>
+ <tile id="12">
+  <image width="58" height="42" source="../graphics/crate.png"/>
+ </tile>
+</tileset>
